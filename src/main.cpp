ac710bac211bef1f91f36edb9e439475d8a94e73
superfufel <alikhan051115@gmail.com>
2026-06-07 07:17:30 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+    int deaths = 0;
+
+    void destroyPlayer(PlayerObject* player, GameObject* obj) {
+        PlayLayer::destroyPlayer(player, obj);
+
+        deaths++;
+
+        if (deaths >= 10) {
+            log::info("RAGE MODE");
+        }
+        else if (deaths >= 5) {
+            log::info("FRUSTRATION
